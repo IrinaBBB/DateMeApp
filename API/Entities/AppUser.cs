72417dffb0041d57650a6b41e6fd4f22bcfd0a33
IrinaB <balkarovairina@gmail.com>
2024-07-20 10:06:36 +0200

    Add Likes Functionality

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -17,5 +17,8 @@
         public string City { get; set; }
         public string Country { get; set; }
         public List<Photo> Photos { get; set; } = new();
+        public List<UserLike> LikedByUsers { get; set; }
+        public List<UserLike> LikedUsers { get; set; }
+
     }
 }
